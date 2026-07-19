5b0985c388595c84da0602ee56661d9a03c660c0
onfarooq0008-eng <onfarooq0008@gmail.com>
2026-07-19 18:09:35 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,28 +1,42 @@
 #!/bin/bash
 
+pkill Xvfb || true
+pkill x11vnc || true
+pkill websockify || true
+
+
 export DISPLAY=:0
+
 
 Xvfb :0 -screen 0 1280x900x24 &
 
-sleep 2
+sleep 3
+
 
 fluxbox &
 
-sleep 2
+sleep 3
+
 
 x11vnc \
 -display :0 \
 -rfbport 5900 \
 -forever \
 -shared \
+-noxdamage \
 -bg
 
-sleep 2
+
+sleep 3
+
 
 websockify \
---web=/usr/share/novnc/ \
+--web=/usr/share/novnc \
 8081 \
 localhost:5900 &
+
+
+sleep 5
 
 
 npm start
